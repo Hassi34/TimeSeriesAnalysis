eaa0562ccd3ea7bd175f912f5a252c337e5f4d5f
Hasnain <hasnainmehmood3435@gmail.com>
2022-01-02 09:40:00 +0300

    .

---
+++ StockAppDescriptive/config.py
@@ -0,0 +1,13 @@
+TWITTER_CONSUMER_KEY = '9ro3g4slh0de5yijy6rqb2p0jgd7hi'
+TWITTER_CONSUMER_SECRET = 'o090sc7828d7gljtrqc5n4vcpx3bfx'
+TWITTER_ACCESS_TOKEN = 'h8bBZtoPNz63S5RkZdbo9R5zb'
+TWITTER_ACCESS_TOKEN_SECRET = 'Jye64vkWxa2dQu64feTnk0BM3j4JO8ZlTa4EQvMDwrweLkwPaw'
+
+TWITTER_USERNAMES = [
+    'traderstewie',
+    'the_chart_life',
+    'canuck2usa',
+    'sunrisetrader',
+    'tmltrader'
+]
+
--- StockPriceForecasting_fbprophet/app.py
+++ StockPriceForecasting_fbprophet/app.py
@@ -1,7 +1,32 @@
+from numpy.core.numeric import ones_like
 import streamlit as st 
-from datetime import date
+import stockForecasting
+import customForecating
+import helper
+st.set_page_config(
+page_title="Copyright © 2021 Hasnain",
+page_icon="🎢",
+layout="wide")
+stocks = stockForecasting.StockForecasting()
+st.sidebar.title('Real-time Forecating App')
+st.sidebar.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROf1dea9H0pSgxQPa_u8vZraz3EtMkeS3K9rjwmlDTJj_WV0WxA_82oa5w-3r7vty8SH0&usqp=CAU')
+option = st.sidebar.radio(
+     'What kind of analysis you want to perform ?',
+     ('Stock Price Prediction', 'Custom Prediction'))
+st.sidebar.write('Current selection is ', f'"{option}"')
 
-import yfinance as yf
-from fbprophet import Prophet 
-from fbprophet.plot import plot_plotly 
-from plotly import graph_objs as go 
+if option == 'Stock Price Prediction' :
+    try: 
+        selected_stock, start_date, end_date, period = stocks.stock_forecast()
+        st.sidebar.button('Make Prediction', on_click=stocks.prediction, args=[selected_stock, start_date, end_date, period])
+    except:
+        st.subheader('Something Went Wrong with the Selection, please make the selection again')
+if option == 'Custom Prediction':
+    customForecating.custom_predictions()
+
+
+st.sidebar.header('Get the Code')
+github_link = '[GitHub](https://github.com/Hassi34/StockPriceForecasting.git)'
+st.sidebar.markdown(github_link, unsafe_allow_html=True)
+portfolio_link = '[My Portfolio](https://hasnainmehmood.pythonanywhere.com/)'
+st.sidebar.markdown(portfolio_link, unsafe_allow_html=True)
